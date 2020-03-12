22ebd7b0e4d2514409575139841910b695f9a6da
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-03-12 09:41:01 +0100

    Adding our Serverless API

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,9 @@
+import AWS from "aws-sdk";
+
+export function call(action, params) {
+  AWS.config.update({ region: "eu-central-1" });
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
